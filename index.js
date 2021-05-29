3e5a77af973c81e0ee4f60fa154735ddb44a4af6
Anonymous <Anonymous@DESKTOP-HOEEBFT>
2021-05-29 14:07:27 +0800

    initials

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ app.get("/*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "views", "index.html"));
 });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server listening on PORT ${port}`));
